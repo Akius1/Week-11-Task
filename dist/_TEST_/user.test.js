fbca1172b2ba358f7d4a2af135440fc7e26622ec
Andrew <andrewurom@gmail.com>
2020-09-28 23:27:18 +0100

    made changes

--- dist/_TEST_/user.test.js
+++ dist/_TEST_/user.test.js
@@ -1,78 +1,79 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const supertest_1 = __importDefault(require("supertest"));
 const app_1 = __importDefault(require("../app"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const request = supertest_1.default(app_1.default);
 describe("Test for all Organizations", () => {
-    it("Get all organizations", (done) => {
-        return request
-            .post("/graphql")
-            .send({
-            query: "{organizations{organization, id, products}}",
-        })
-            .set("Accept", "Application/json")
-            .expect("Content-Type", /json/)
-            .then(async (res) => {
-            const datas = await res.body;
-            expect(datas).toBeInstanceOf(Object);
-            expect(datas.data.organizations.length).toEqual(16);
-            done();
-        });
-    });
-    it("Returns organizations with organization", async (done) => {
-        return await request
-            .post("/graphql")
-            .send({
-            query: '{ getOneByID(id:"5f6754e2e3a6243fd89d36e9") { organization, ceo, marketValue} }',
-        })
-            .expect(200)
-            .end((err, res) => {
-            if (err)
-                return done(err);
-            expect(res.body.data.getOneByID).toHaveProperty("organization");
-            expect(res.body.data.getOneByID).toHaveProperty("ceo");
-            expect(res.body.data.getOneByID).toHaveProperty("marketValue");
-            done();
-        });
-    });
+  it("Get all organizations", (done) => {
+    return request
+      .post("/graphql")
+      .send({
+        query: "{organizations{organization, id, products}}",
+      })
+      .set("Accept", "Application/json")
+      .expect("Content-Type", /json/)
+      .then(async (res) => {
+        const datas = await res.body;
+        expect(datas).toBeInstanceOf(Object);
+        expect(datas.data.organizations.length).toEqual(16);
+        done();
+      });
+  });
+  it("Returns organizations with organization", (done) => {
+    return request
+      .post("/graphql")
+      .send({
+        query:
+          '{ getOneByID(id:"5f6754e2e3a6243fd89d36e9") { organization, ceo, marketValue} }',
+      })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.data.getOneByID).toHaveProperty("organization");
+        expect(res.body.data.getOneByID).toHaveProperty("ceo");
+        expect(res.body.data.getOneByID).toHaveProperty("marketValue");
+        done();
+      });
+  });
 });
-describe("Test for all Users", async () => {
-    it("Get all users", (done) => {
-        return await request
-            .post("/graphql")
-            .send({
-            query: "{getUsers{firstName, lastName, email}}",
-        })
-            .set("Accept", "Application/json")
-            .expect("Content-Type", /json/)
-            .end(function (err, res) {
-            if (err)
-                return done(err);
-            expect(res.body).toBeInstanceOf(Object);
-            expect(res.body.data.getUsers.length).toEqual(10);
-            done();
-        });
-    });
-    it("Returns users by Id", async (done) => {
-        return await request
-            .post("/graphql")
-            .send({
-            query: '{ getAUserByID(email:"ugoDav@gmail.com", password:"helloWorld") { email, firstName, lastName} }',
-        })
-            .expect(200)
-            .end((err, res) => {
-            if (err)
-                return done(err);
-            expect(res.body.data.getAUserByID).toHaveProperty("email");
-            expect(res.body.data.getAUserByID).toHaveProperty("firstName");
-            expect(res.body.data.getAUserByID).toHaveProperty("lastName");
-            done();
-        });
-    });
+describe("Test for all Users", () => {
+  it("Get all users", (done) => {
+    return request
+      .post("/graphql")
+      .send({
+        query: "{getUsers{firstName, lastName, email}}",
+      })
+      .set("Accept", "Application/json")
+      .expect("Content-Type", /json/)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.body).toBeInstanceOf(Object);
+        expect(res.body.data.getUsers.length).toEqual(10);
+        done();
+      });
+  });
+  it("Returns users by Id", (done) => {
+    return request
+      .post("/graphql")
+      .send({
+        query:
+          '{ getAUserByID(email:"ugoDav@gmail.com", password:"helloWorld") { email, firstName, lastName} }',
+      })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.data.getAUserByID).toHaveProperty("email");
+        expect(res.body.data.getAUserByID).toHaveProperty("firstName");
+        expect(res.body.data.getAUserByID).toHaveProperty("lastName");
+        done();
+      });
+  });
 });
 // describe("Test for Mutation", () => {
 //   it("Add an organization", async (done) => {
@@ -93,6 +94,6 @@ describe("Test for all Users", async () => {
 //   });
 // });
 afterAll((done) => {
-    mongoose_1.default.connection.close();
-    done();
+  mongoose_1.default.connection.close();
+  done();
 });
